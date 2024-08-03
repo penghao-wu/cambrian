91c46bf5bd273fbb64f09a70c243f4a681d7ba9c
penghaowu <wupenghaocraig@gmail.com>
2024-08-04 00:35:51 +0800

    debug

--- pretrain_cambrian_7b_2tower_connectoronly_short.sh
+++ pretrain_cambrian_7b_2tower_connectoronly_short.sh
@@ -3,7 +3,7 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian-connectoronly-D5-G1-2tower_7b-pretrain_1.2m" &&
+export CKPT_NAME="cambrian-connectoronly-D3-G4-2tower_7b-pretrain_1.2m" &&
 
 export CKPT_DIR="gs://us-central2-storage/cambrian/checkpoints/$CKPT_NAME" &&
 
@@ -15,9 +15,9 @@ python cambrian/train/train_tpu.py \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336", "clip-convnext-L-multi-stage"]' \
     --vision_tower_aux_token_len_list '[576, 9216]' \
     --image_token_len 576 \
-    --num_query_group 1 \
-    --query_num_list '[576]' \
-    --connector_depth 5 \
+    --num_query_group 4 \
+    --query_num_list '[576, 576, 576, 576]' \
+    --connector_depth 3 \
     --image_position 35 \
     --vision_hidden_size 1024 \
     --connector_only True \
