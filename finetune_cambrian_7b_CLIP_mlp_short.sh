7059f6e6305dc0d3274f4d9ef65d5c240d428f08
penghaowu <wupenghaocraig@gmail.com>
2024-08-12 19:48:03 +0800

    debug

--- finetune_cambrian_7b_CLIP_mlp_short.sh
+++ finetune_cambrian_7b_CLIP_mlp_short.sh
@@ -3,21 +3,21 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian_7b_CLIP_mlp_576_finetune_737k" &&
+export CKPT_NAME="cambrian_7b_CLIP_mlp_36_im_nocausal_finetune_737k" &&
 
-export CKPT_DIR="gs://us-central2-storage/cambrian/checkpoints/$CKPT_NAME" &&
+export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
 python cambrian/train/train_tpu.py \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
     --data_path /mnt/disks/storage/data/finetune_data/jsons/737k.jsonl \
     --image_folder /mnt/disks/storage/data/finetune_data \
-    --pretrain_mm_mlp_adapter ./cambrian_7b_CLIP_mlp_576_shareGPT4V_pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./cambrian_7b_CLIP_mlp_36_im_nocausal_shareGPT4V_pretrain/mm_projector.bin \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
-    --vision_tower_aux_token_len_list '[576]' \
-    --image_token_len 576 \
+    --vision_tower_aux_token_len_list '[36]' \
+    --image_token_len 36 \
     --num_query_group 1 \
-    --query_num_list '[576]' \
+    --query_num_list '[36]' \
     --connector_depth 3 \
     --image_position 35 \
     --vision_hidden_size 1024 \
@@ -65,5 +65,5 @@ if [ ! -d "$CKPT_PATH" ]; then
     exit 1
 fi
 echo "Training finished. Syncing checkpoints to GCS..."
-gcloud alpha storage rsync $CKPT_PATH gs://us-central2-storage/cambrian/checkpoints/$CKPT_NAME
-echo "Syncing finished. Checkpoints are now available at gs://us-central2-storage/cambrian/checkpoints/$CKPT_NAME"
+gcloud alpha storage rsync $CKPT_PATH gs://cambrian-archive/checkpoints/$CKPT_NAME
+echo "Syncing finished. Checkpoints are now available at gs://cambrian-archive/checkpoints/$CKPT_NAME"
