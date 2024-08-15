8ea4b346fdc22997f843b6f46af66be0803515aa
penghaowu <wupenghaocraig@gmail.com>
2024-08-15 23:31:03 +0800

    debug

--- cambrian/train/train_fsdp.py
+++ cambrian/train/train_fsdp.py
@@ -1167,7 +1167,7 @@ def prepare_multimodal_data(input_ids, labels, attention_mask, image_sizes, imag
             if cur_labels_im_replaced[index_i] != IGNORE_INDEX:
                 first_answer_index = index_i
                 break
-        gist_token_positions.append(first_answer_index)
+        gist_token_positions.append(first_answer_index-1)
         # cur_attention_mask_im_part = cur_attention_mask_im_replaced[image_token_index:first_answer_index]
         # cur_attention_mask_im_part_binary = cur_attention_mask_im_part.view(1, 1, -1).repeat(1, image_token_len_with_newline, 1)
         # cur_attention_mask_im_part = torch.zeros_like(cur_attention_mask_im_part_binary)
