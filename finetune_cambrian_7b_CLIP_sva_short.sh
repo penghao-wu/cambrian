3ecd998ac4a15e74e2f660276204e0e1dfae48e5
penghaowu <wupenghaocraig@gmail.com>
2024-08-16 14:12:52 +0800

    debug

--- finetune_cambrian_7b_CLIP_sva_short.sh
+++ finetune_cambrian_7b_CLIP_sva_short.sh
@@ -3,7 +3,7 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian_7b_CLIP_sva_1G_LLM10_36_gist_token_25instpt_finetune_737k" &&
+export CKPT_NAME="cambrian_7b_CLIP_sva_1G_LLM10_36_25instpt_finetune_737k" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -12,7 +12,7 @@ python cambrian/train/train_tpu.py \
     --version v1 \
     --data_path /mnt/disks/storage/data/finetune_data/jsons/737k.jsonl \
     --image_folder /mnt/disks/storage/data/finetune_data \
-    --pretrain_mm_mlp_adapter ./cambrian_7b_CLIP_sva_1G_LLM10_36_gist_token_25inst_pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./cambrian_7b_CLIP_sva_1G_LLM10_36_25inst_pretrain/mm_projector.bin \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
     --image_token_len 36 \
