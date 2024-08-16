157b29c218729417b67da3111f7b558035024c4a
penghaowu <wupenghaocraig@gmail.com>
2024-08-16 20:25:24 +0800

    debug

--- cambrian/model/language_model/cambrian_llama.py
+++ cambrian/model/language_model/cambrian_llama.py
@@ -183,7 +183,7 @@ class CambrianLlamaModel(CambrianMetaModel, LlamaModel):
 
 			hidden_states = layer_outputs[0]
 
-			vision_tokens = self.vision_sampler_layers[i](vision_tokens)
+			# vision_tokens = self.vision_sampler_layers[i](vision_tokens)
 
 			hidden_states[:, latent_query_start_idx:latent_query_start_idx+latent_query_newline_num, :] = vision_tokens
 
