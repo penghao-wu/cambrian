39baf9947ab5079bf66464f0127612a8e5395ec0
penghaowu <wupenghaocraig@gmail.com>
2024-08-17 22:33:01 +0800

    debug

--- cambrian/train/train_fsdp.py
+++ cambrian/train/train_fsdp.py
@@ -1199,8 +1199,8 @@ def prepare_multimodal_data(input_ids, labels, attention_mask, image_sizes, imag
         cur_attention_mask_c2f[:, image_position:image_position+image_token_len_concise_with_newline, image_position:image_position+image_token_len_concise_with_newline] = cur_im_attention_mask_concise # concise to concise
 
         # text to all
-        cur_attention_mask_c2f[:, image_position+image_token_len_concise_with_newline:, :image_position] = cur_attention_mask_im_replaced[:, image_position:image_position+image_token_len_with_newline, :image_position] # text to sys
-        cur_attention_mask_c2f[:, image_position+image_token_len_concise_with_newline:, image_position+image_token_len_concise_with_newline:] = cur_attention_mask_im_replaced[:, image_position:image_position+image_token_len_with_newline, image_position:] # text to full+text
+        cur_attention_mask_c2f[:, image_position+image_token_len_concise_with_newline:, :image_position] = cur_attention_mask_im_replaced[:, image_position+image_token_len_with_newline:, :image_position] # text to sys
+        cur_attention_mask_c2f[:, image_position+image_token_len_concise_with_newline:, image_position+image_token_len_concise_with_newline:] = cur_attention_mask_im_replaced[:, image_position+image_token_len_with_newline:, image_position:] # text to full+text
         
         input_ids_im_replaced.append(cur_input_ids_im_replaced)
         labels_im_replaced.append(cur_labels_im_replaced)
