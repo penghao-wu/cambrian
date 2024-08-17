5d97fea7302c9751b30bc7f66045f5894d21bd96
penghaowu <wupenghaocraig@gmail.com>
2024-08-17 23:43:12 +0800

    debug

--- pretrain_cambrian_7b_CLIP_mlp_short.sh
+++ pretrain_cambrian_7b_CLIP_mlp_short.sh
@@ -3,7 +3,7 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian_7b_CLIP_mlp_2scale_576_576_shareGPT4V_pretrain" &&
+export CKPT_NAME="cambrian_7b_CLIP_mlp_2scale_36_36_shareGPT4V_pretrain" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -15,7 +15,7 @@ python cambrian/train/train_tpu.py \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
     --image_token_len 576 \
-    --image_token_len_concise 576 \
+    --image_token_len_concise 36 \
     --num_query_group 1 \
     --query_num_list '[576]' \
     --connector_depth 3 \
@@ -48,7 +48,7 @@ python cambrian/train/train_tpu.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 False \
-    --model_max_length 2648 \
+    --model_max_length 2006 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
