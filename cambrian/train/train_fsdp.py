eb95013427e9caffc05f65ee2d901b4b359f3a86
penghaowu <wupenghaocraig@gmail.com>
2024-08-17 08:10:51 +0800

    debug

--- cambrian/train/train_fsdp.py
+++ cambrian/train/train_fsdp.py
@@ -1171,11 +1171,11 @@ def prepare_multimodal_data(input_ids, labels, attention_mask, image_sizes, imag
         # cur_attention_mask_im_part = cur_attention_mask_im_replaced[image_token_index:first_answer_index]
         # cur_attention_mask_im_part_binary = cur_attention_mask_im_part.view(1, 1, -1).repeat(1, image_token_len_with_newline, 1)
         # cur_attention_mask_im_part = torch.zeros_like(cur_attention_mask_im_part_binary)
-        # min_dtype = torch.finfo(torch.bfloat16).min
+        min_dtype = torch.finfo(torch.bfloat16).min
         # cur_attention_mask_im_part = cur_attention_mask_im_part.masked_fill(cur_attention_mask_im_part_binary.eq(0.0), min_dtype)
 
         cur_attention_mask_im_replaced = combine_causal_attention_mask(len(cur_attention_mask_im_replaced), cur_attention_mask_im_replaced)
-        # cur_attention_mask_im_replaced[:, image_token_index:image_token_end_index, first_answer_index-1:first_answer_index] = 0
+        cur_attention_mask_im_replaced[:, image_token_index:image_token_end_index, :image_token_index] = min_dtype
         
         input_ids_im_replaced.append(cur_input_ids_im_replaced)
         labels_im_replaced.append(cur_labels_im_replaced)
