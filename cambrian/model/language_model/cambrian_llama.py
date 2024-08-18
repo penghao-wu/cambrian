7109aed26bae70e335072fe54865b6ae0ca8930e
penghaowu <wupenghaocraig@gmail.com>
2024-08-18 11:14:29 +0800

    debug

--- cambrian/model/language_model/cambrian_llama.py
+++ cambrian/model/language_model/cambrian_llama.py
@@ -162,11 +162,16 @@ class CambrianLlamaModel(CambrianMetaModel, LlamaModel):
 					use_cache,
 				)
 
-			assert torch.allclose(layer_outputs[0], layer_outputs_1[0], atol=1e-05), (torch.allclose(layer_outputs[0][:, vision_token_start_idx:vision_token_start_idx+image_token_concise_newline_num], layer_outputs_1[0][:, vision_token_start_idx:vision_token_start_idx+image_token_concise_newline_num], atol=1e-05), torch.allclose(layer_outputs[0][:, vision_token_start_idx+image_token_concise_newline_num:], layer_outputs_1[0][:, vision_token_start_idx+image_token_concise_newline_num:], atol=1e-05), torch.allclose(layer_outputs[0][:, :vision_token_start_idx], layer_outputs_1[0][:, :vision_token_start_idx], atol=1e-05))
-
 			hidden_states_vision_concise = layer_outputs[0][:, vision_token_start_idx:vision_token_start_idx+image_token_concise_newline_num]
 			hidden_states_text = layer_outputs[0][:, vision_token_start_idx+image_token_concise_newline_num:]
 			hidden_states_sys = layer_outputs[0][:, :vision_token_start_idx]
+
+
+			hidden_states_vision_concise_1 = layer_outputs_1[0][:, vision_token_start_idx:vision_token_start_idx+image_token_concise_newline_num]
+			hidden_states_text_1 = layer_outputs_1[0][:, vision_token_start_idx+image_token_concise_newline_num:]
+			hidden_states_sys_1 = layer_outputs_1[0][:, :vision_token_start_idx]
+
+			assert False, ((hidden_states_vision_concise-hidden_states_vision_concise_1).max(), (hidden_states_text-hidden_states_text_1).max(), (hidden_states_sys-hidden_states_sys_1).max())
 
 			hidden_states_vision_full = hidden_states_vision_concise
 			# # update vision full with concise
