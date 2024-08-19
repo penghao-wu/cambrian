68623b96cbb4d27147ceddbd1b52cfe1e2a5fdd9
penghaowu <wupenghaocraig@gmail.com>
2024-08-19 16:15:38 +0800

    debug

--- pretrain_cambrian_7b_CLIP_mlp_short.sh
+++ pretrain_cambrian_7b_CLIP_mlp_short.sh
@@ -3,7 +3,7 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian_7b_CLIP_mlp_2scale_postln_576_36_shareGPT4V_pretrain_lr1e4" &&
+export CKPT_NAME="cambrian_7b_CLIP_mlp_2scale_postln_576_36_shareGPT4V_pretrain_lr1e4_fix" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -48,7 +48,7 @@ python cambrian/train/train_tpu.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 False \
-    --model_max_length 1976 \
+    --model_max_length 2006 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
