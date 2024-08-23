7fcb3eee279b3f77ee4ab005dca71b7f6ae30dad
penghaowu <wupenghaocraig@gmail.com>
2024-08-23 21:31:58 +0800

    debug

--- cambrian/model/language_model/cambrian_llama.py
+++ cambrian/model/language_model/cambrian_llama.py
@@ -119,8 +119,8 @@ class CambrianLlamaModel(CambrianMetaModel, LlamaModel):
 		# skip_layers = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 		# skip_layers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 		skip_layers = [_ for _ in range(12, 32)]
-		skip_layers = [_ for _ in range(21, 32)]
-		skip_layers += [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+		# skip_layers = [_ for _ in range(21, 32)]
+		# skip_layers += [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
 		for i, decoder_layer in enumerate(self.layers):
 			if output_hidden_states:
