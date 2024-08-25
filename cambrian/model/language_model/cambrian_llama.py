aadfa0d8a696c025c3cfa7befffa9cfd6ef2bb70
penghaowu <wupenghaocraig@gmail.com>
2024-08-25 16:09:48 +0800

    debug

--- cambrian/model/language_model/cambrian_llama.py
+++ cambrian/model/language_model/cambrian_llama.py
@@ -119,7 +119,7 @@ class CambrianLlamaModel(CambrianMetaModel, LlamaModel):
 		skip_layers = [_ for _ in range(0, 32)]
 		# skip_layers = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 		# skip_layers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-		skip_layers = [_ for _ in range(6, 32)]
+		skip_layers = [_ for _ in range(0, 32)]
 		# skip_layers = [_ for _ in range(21, 32)]
 		# skip_layers += [0, 1, 2, 3, 4, 5]
 
