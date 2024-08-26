4d7620aee9fb05a39b086b029372c95a45daa055
penghaowu <wupenghaocraig@gmail.com>
2024-08-26 14:16:20 +0800

    debug

--- cambrian/model/vision_sampler.py
+++ cambrian/model/vision_sampler.py
@@ -549,7 +549,7 @@ class VisionSA(nn.Module):
 		input_embed = input_embed[:, :, :-1].view(bs, side_len_input, side_len_input, -1)
 		
 		input_embed = input_embed.view(bs, side_len_context, reduce_factor, side_len_context, reduce_factor, -1).permute(0, 1, 3, 2, 4, 5).contiguous().flatten(0, 2).flatten(1, 2)
-		residual = input_embed.flatten(1,2)
+		residual = input_embed
 
 		context_newline = context[:, :, -1:]
 		context = context[:, :, :-1].view(bs, side_len_context, side_len_context, 1, 1, -1).repeat(1, 1, 1, 1, 1, 1).flatten(0, 2).flatten(1, 2)
@@ -563,6 +563,8 @@ class VisionSA(nn.Module):
 
 		sa_kv = torch.cat([input_embed, context], dim=1)
 		input_embed = self.self_attention(sa_kv, input_embed, attention_masks) + residual
+
+		input_embed = input_embed.view(bs, side_len_context, side_len_context, reduce_factor, reduce_factor, -1).permute(0, 1, 3, 2, 4, 5).contiguous().view(bs, side_len_input, side_len_input, -1)
 
 		input_embed_newline = torch.repeat_interleave(context_newline, reduce_factor, 1)
 
