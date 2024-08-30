9d47387d952cf8b80f1066aef0d6c309cc14ddd3
penghaowu <wupenghaocraig@gmail.com>
2024-08-30 10:21:52 +0800

    debug

--- cambrian/model/vision_sampler.py
+++ cambrian/model/vision_sampler.py
@@ -453,12 +453,14 @@ class VisionMLP(nn.Module):
 
 		input_embed = input_embed.view(bs, side_len_input, side_len_input+1, -1)
 		context = context.view(bs, side_len_context, side_len_context+1, -1)
+		context_residual = context_residual.view(bs, side_len_context, side_len_context+1, -1)
 
 		input_embed = input_embed[:, :, :-1].view(bs, side_len_input, side_len_input, -1)
 		input_embed = input_embed.view(bs, side_len_context, reduce_factor, side_len_context, reduce_factor, -1).permute(0, 1, 3, 2, 4, 5).contiguous().flatten(1, 4)
 
 		context_newline = context[:, :, -1:]
 		context = context[:, :, :-1].view(bs, side_len_context, side_len_context, 1, 1, -1).repeat(1, 1, 1, reduce_factor, reduce_factor, 1).flatten(1, 4)
+		context_residual = context_residual[:, :, :-1].view(bs, side_len_context, side_len_context, 1, 1, -1).repeat(1, 1, 1, reduce_factor, reduce_factor, 1).flatten(1, 4)
 
 		context = self.context_proj(context)
 		residual = input_embed
