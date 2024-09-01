ba914e213f0945b35e6a0c0591c4e03c9b403ba4
penghaowu <wupenghaocraig@gmail.com>
2024-09-01 18:31:58 +0800

    debug

--- cambrian/train/train_fsdp.py
+++ cambrian/train/train_fsdp.py
@@ -1748,7 +1748,7 @@ def train(INDEX, attn_implementation=None):
 	trainer = CambrianTrainer(model=model,
 					tokenizer=tokenizer,
 					args=training_args,
-					extra_losses=["lm_loss", "aux_loss"],
+					# extra_losses=["lm_loss", "aux_loss"],
 					**data_module)
 	trainer.is_fsdp_enabled = True
 	if training_args.train_continue:
