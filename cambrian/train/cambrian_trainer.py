5806aa6d77984ed00b763527dfdbf8fb0093b019
penghaowu <wupenghaocraig@gmail.com>
2024-09-01 20:33:06 +0800

    debug

--- cambrian/train/cambrian_trainer.py
+++ cambrian/train/cambrian_trainer.py
@@ -197,24 +197,73 @@ def map_params_to_module_names(model_list):
                 param_to_name[param] = f"{module_name}.{param_name}"
     return param_to_name
 
+
+from torch.utils.data import Dataset, DataLoader
+from transformers.modeling_utils import unwrap_model
+
 class CambrianTrainer(CustomTrainer):
 
-    def compute_loss(self, model, inputs, return_outputs=False):
-        print(self.control.extra_losses, flush=True)
-        if hasattr(self.control, 'extra_losses') and model.training:
-            loss, outputs = super(CustomTrainer, self).compute_loss(model, inputs, return_outputs=True)
+    def _maybe_log_save_evaluate(self, tr_loss, model, trial, epoch, ignore_keys_for_eval):
+        """ adapted from Trainer._maybe_log_save_evaluate to support logging extra losses
+        """
+        if self.control.should_log and self.state.global_step > self._globalstep_last_logged:
+            if is_torch_tpu_available():
+                xm.mark_step()
 
-            if not isinstance(outputs, dict):
-                raise ValueError("The model output should be a dictionary or ModelOutput and not a tuple or list.")
-            for k, v in outputs.items():
-                if k in self.control.extra_losses:
-                    if v is not None:
-                        if self.args.n_gpu > 1:
-                            v = v.mean()
-                        self.control.extra_losses[k] += v.detach() / self.args.gradient_accumulation_steps
-            return (loss, outputs) if return_outputs else loss
-        else:
-            return super(CustomTrainer, self).compute_loss(model, inputs, return_outputs=return_outputs)
+            logs: Dict[str, float] = {}
+
+            # all_gather + mean() to get average loss over all processes
+            tr_loss_scalar = self._nested_gather(tr_loss).mean().item()
+
+            # reset tr_loss to zero
+            tr_loss -= tr_loss
+
+            logs["loss"] = round(tr_loss_scalar / (self.state.global_step - self._globalstep_last_logged), 4)
+
+            if hasattr(self.control, 'extra_losses'):
+                for k, v in self.control.extra_losses.items():
+                    logs[k] = self._nested_gather(v).mean().item()
+                    # reset the loss
+                    self.control.extra_losses[k] -= self.control.extra_losses[k]
+
+                    logs[k] = round(logs[k] / (self.state.global_step - self._globalstep_last_logged), 4)
+
+            logs["learning_rate"] = self._get_learning_rate()
+
+            logs.update(unwrap_model(model).get_extra_logging_dict()
+                        if hasattr(unwrap_model(model), 'get_extra_logging_dict') else {})
+
+            self._total_loss_scalar += tr_loss_scalar
+            self._globalstep_last_logged = self.state.global_step
+            self.store_flos()
+            print(logs, flush=True)
+            self.log(logs)
+
+        metrics = None
+        if self.control.should_evaluate:
+            if isinstance(self.eval_dataset, dict):
+                metrics = {}
+                for eval_dataset_name, eval_dataset in self.eval_dataset.items():
+                    dataset_metrics = self.evaluate(
+                        eval_dataset=eval_dataset,
+                        ignore_keys=ignore_keys_for_eval,
+                        metric_key_prefix=f"eval_{eval_dataset_name}",
+                    )
+                    metrics.update(dataset_metrics)
+            else:
+                metrics = self.evaluate(ignore_keys=ignore_keys_for_eval)
+            self._report_to_hp_search(trial, self.state.global_step, metrics)
+
+            # Run delayed LR scheduler now that metrics are populated
+            if isinstance(self.lr_scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                metric_to_check = self.args.metric_for_best_model
+                if not metric_to_check.startswith("eval_"):
+                    metric_to_check = f"eval_{metric_to_check}"
+                self.lr_scheduler.step(metrics[metric_to_check])
+
+        if self.control.should_save:
+            self._save_checkpoint(model, trial, metrics=metrics)
+            self.control = self.callback_handler.on_save(self.args, self.state, self.control)
 
     def _get_train_sampler(self) -> Optional[torch.utils.data.Sampler]:
         if self.train_dataset is None or not has_length(self.train_dataset):
