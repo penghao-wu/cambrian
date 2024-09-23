2f128c6d3385860265081f501a15f794837e368a
penghaowu <wupenghaocraig@gmail.com>
2024-09-23 12:55:47 +0800

    debug

--- finetune_cambrian_qwen05b_CLIP_mlp.sh
+++ finetune_cambrian_qwen05b_CLIP_mlp.sh
@@ -3,7 +3,7 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian_qwen05b_CLIP_mlp_2scale_dim448_skip12_24_finetune_737k" &&
+export CKPT_NAME="cambrian_qwen05b_CLIP_mlp_2scale_dim448_skip8_24_finetune_737k" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -12,7 +12,7 @@ python cambrian/train/train_tpu.py \
     --version qwen_1_5 \
     --data_path /mnt/disks/storage/data/finetune_data/jsons/737k.jsonl \
     --image_folder /mnt/disks/storage/data/finetune_data \
-    --pretrain_mm_mlp_adapter ./cambrian_qwen05b_CLIP_mlp_2scale_dim448_skip12_24_shareGPT4V_pretrain_lr1e4/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./cambrian_qwen05b_CLIP_mlp_2scale_dim448_skip8_24_shareGPT4V_pretrain_lr1e4/mm_projector.bin \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
     --image_token_len 576 \
