7699a3b3d109065e910174572ace5a4c4f620f08
penghaowu <wupenghaocraig@gmail.com>
2024-09-25 16:20:30 +0800

    debug

--- cambrian/model/cambrian_arch.py
+++ cambrian/model/cambrian_arch.py
@@ -341,6 +341,9 @@ class CambrianMetaForCausalLM(ABC):
         image_aux_features_list = []
         for image_aux, vision_tower_aux in zip(image_aux_list, vision_tower_aux_list):
             image_aux_features = vision_tower_aux(image_aux)
+            data_dict = {'image':image_aux.detach().cpu(), 'image_feature':image_aux_features.detach().cpu()}
+            torch.save('data_main.pth', data_dict)
+            assert False
             image_aux_features_list.append(image_aux_features)
         return image_aux_features_list
 
