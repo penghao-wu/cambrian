a516d33d1c59054b7045beb51d068667657b9cfc
penghaowu <wupenghaocraig@gmail.com>
2024-09-25 14:52:06 +0800

    debug

--- cambrian/model/language_model/cambrian_llama.py
+++ cambrian/model/language_model/cambrian_llama.py
@@ -359,7 +359,7 @@ class CambrianLlamaForCausalLM(LlamaForCausalLM, CambrianMetaForCausalLM):
 			# Enable model parallelism
 			shift_labels = shift_labels.to(shift_logits.device)
 			loss = loss_fct(shift_logits, shift_labels)
-			assert False, (shift_labels[576:], loss)
+			assert False, (shift_labels[576:], loss, shift_logits[576:])
 
 		if not return_dict:
 			output = (logits,) + outputs[1:]
