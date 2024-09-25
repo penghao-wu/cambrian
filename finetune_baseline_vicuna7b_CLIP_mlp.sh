a9e570e7508d0e9344e19e7a13e0d5e3278181eb
penghaowu <wupenghaocraig@gmail.com>
2024-09-25 20:00:29 +0800

    debug

--- finetune_baseline_vicuna7b_CLIP_mlp.sh
+++ finetune_baseline_vicuna7b_CLIP_mlp.sh
@@ -5,8 +5,8 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export CKPT_NAME="compressv_vicuna7b_CLIP_mlp_baseline_finetune_737k_debug" &&
 
-export TPU_PROCESS_BOUNDS=1,1,1 &&
-export TPU_VISIBLE_CHIPS=0 &&
+# export TPU_PROCESS_BOUNDS=1,1,1 &&
+# export TPU_VISIBLE_CHIPS=0 &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -54,7 +54,7 @@ python cambrian/train/train_tpu.py \
     --tf32 False \
     --model_max_length 626 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 0 \
+    --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb \
     --run_name $CKPT_NAME \
