bc739bad34b479b3323d0b26861f8f74cc891450
penghaowu <wupenghaocraig@gmail.com>
2024-09-25 21:52:21 +0800

    debug

--- cambrian/model/language_model/cambrian_llama.py
+++ cambrian/model/language_model/cambrian_llama.py
@@ -169,6 +169,8 @@ class CambrianLlamaModel(CambrianMetaModel, LlamaModel):
 
 		# skip_layers = [_ for _ in range(0, 32)]
 		skip_layers = []
+		data_dict = {"hidden_states":hidden_states.detach().cpu(),"attention_mask":attention_mask.detach().cpu(),"position_ids":position_ids.detach().cpu()}
+		torch.save(data_dict, 'input_main.pth')
 
 		for i, decoder_layer in enumerate(self.layers):
 			if output_hidden_states:
