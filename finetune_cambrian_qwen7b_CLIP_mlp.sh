f15024cc373cf248466c8a15ce404c5bd7ebef5b
penghaowu <wupenghaocraig@gmail.com>
2024-09-26 22:14:43 +0800

    debug

--- finetune_cambrian_qwen7b_CLIP_mlp.sh
+++ finetune_cambrian_qwen7b_CLIP_mlp.sh
@@ -10,8 +10,8 @@ export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 python cambrian/train/train_tpu.py \
     --model_name_or_path "Qwen/Qwen2-0.5B-Instruct" \
     --version qwen_1_5 \
-    --data_path /mnt/disks/storage/data/finetune_data/jsons/737k.jsonl \
-    --image_folder /mnt/disks/storage/data/finetune_data \
+    --data_path ./llava_next_raw_format_processed.jsonl\
+    --image_folder ./llava_next \
     --pretrain_mm_mlp_adapter ./llava15-openai_clip-vit-large-patch14-336-Qwen_Qwen2-0.5B-Instruct-mlp2x_gelu-pretrain_sharegpt4v_plain_bs512/mm_projector.bin \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
