9f0f952a17607a4e9d3f73978ffcacd295304af8
penghaowu <wupenghaocraig@gmail.com>
2024-09-27 08:07:03 +0800

    debug

--- finetune_cambrian_7b_CLIP_mlp_short.sh
+++ finetune_cambrian_7b_CLIP_mlp_short.sh
@@ -3,7 +3,7 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian_7b_CLIP_mlp_576_finetune_737k_nounmask_debug" &&
+export CKPT_NAME="cambrian_7b_CLIP_mlp_576_finetune_738k_nounmask_new" &&
 
 # export TPU_PROCESS_BOUNDS=1,1,1 &&
 # export TPU_VISIBLE_CHIPS=0 &&
@@ -13,8 +13,8 @@ export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 python cambrian/train/train_tpu.py \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path /mnt/disks/storage/data/finetune_data/jsons/737k.jsonl \
-    --image_folder /mnt/disks/storage/data/finetune_data \
+    --data_path ./llava_next_raw_format_processed.jsonl\
+    --image_folder ./llava_next \
     --pretrain_mm_mlp_adapter ./compressv_vicuna7b_CLIP_mlp_baseline_shareGPT4V_pretrain/mm_projector.bin \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
@@ -61,12 +61,12 @@ python cambrian/train/train_tpu.py \
     --fsdp_config fsdp_config.json
 
 
-CKPT_PATH=checkpoints/$CKPT_NAME
-# check if the checkpoint path exists
-if [ ! -d "$CKPT_PATH" ]; then
-    echo "Checkpoint path does not exist. Exiting..."
-    exit 1
-fi
-echo "Training finished. Syncing checkpoints to GCS..."
-gcloud alpha storage rsync $CKPT_PATH gs://cambrian-archive/checkpoints/$CKPT_NAME
-echo "Syncing finished. Checkpoints are now available at gs://cambrian-archive/checkpoints/$CKPT_NAME"
+# CKPT_PATH=checkpoints/$CKPT_NAME
+# # check if the checkpoint path exists
+# if [ ! -d "$CKPT_PATH" ]; then
+#     echo "Checkpoint path does not exist. Exiting..."
+#     exit 1
+# fi
+# echo "Training finished. Syncing checkpoints to GCS..."
+# gcloud alpha storage rsync $CKPT_PATH gs://cambrian-archive/checkpoints/$CKPT_NAME
+# echo "Syncing finished. Checkpoints are now available at gs://cambrian-archive/checkpoints/$CKPT_NAME"
