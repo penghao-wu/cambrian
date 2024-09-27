774a1f4aa7428f010f96dca9fdac0515a56beb71
penghaowu <wupenghaocraig@gmail.com>
2024-09-27 22:59:13 +0800

    debug

--- pretrain_cambrian_qwen05b_CLIP_mlp_short.sh
+++ pretrain_cambrian_qwen05b_CLIP_mlp_short.sh
@@ -3,7 +3,7 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian_qwen05b_CLIP_mlp_2scale_dim448_skip12_24_shareGPT4V_pretrain_lr1e4_old_nounpad" &&
+export CKPT_NAME="cambrian_qwen05b_CLIP_mlp_2scale_dim448_skip12_24_shareGPT4V_pretrain_lr1e4_old" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
