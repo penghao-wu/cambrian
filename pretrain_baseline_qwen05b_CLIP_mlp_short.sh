56319e514600a911ea8d7a0a205f0f39b03c1723
penghaowu <wupenghaocraig@gmail.com>
2024-09-28 23:16:54 +0800

    debug

--- pretrain_baseline_qwen05b_CLIP_mlp_short.sh
+++ pretrain_baseline_qwen05b_CLIP_mlp_short.sh
@@ -3,7 +3,7 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="compressv_qwen05b_CLIP_mlp_baseline_shareGPT4V_square_pretrain" &&
+export CKPT_NAME="compressv_qwen05b_CLIP_mlp_baseline_shareGPT4V_resize_pretrain" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
