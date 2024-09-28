1ebe04c06177d2b073ba32ae7b8ea1484e0a6e70
penghaowu <wupenghaocraig@gmail.com>
2024-09-28 20:06:09 +0800

    debug

--- pretrain_baseline_qwen05b_CLIP_mlp_short.sh
+++ pretrain_baseline_qwen05b_CLIP_mlp_short.sh
@@ -31,7 +31,7 @@ python cambrian/train/train_tpu.py \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_aspect_ratio square \
+    --image_aspect_ratio pad \
     --bf16 False \
     --output_dir $CKPT_DIR \
     --num_train_epochs 1 \
