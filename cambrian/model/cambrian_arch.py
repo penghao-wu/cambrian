9864c17f64f360ddd2a75804cf68b14abe7a1f33
penghaowu <wupenghaocraig@gmail.com>
2024-09-28 16:56:46 +0800

    debug

--- cambrian/model/cambrian_arch.py
+++ cambrian/model/cambrian_arch.py
@@ -225,8 +225,8 @@ class CambrianMetaModel:
                     self.vision_sampler_layers.load_state_dict(get_w(mm_projector_weights, 'vision_sampler_layers'),strict=True)
                 self.vision_query.data = mm_projector_weights['model.vision_query']
             self.image_newline.data = mm_projector_weights['model.image_newline']
-            # if compress_v:
-            #     self.vision_mlp_layers.load_state_dict(get_w(mm_projector_weights, 'vision_mlp_layers'),strict=True)
+            if compress_v:
+                self.vision_mlp_layers.load_state_dict(get_w(mm_projector_weights, 'vision_mlp_layers'),strict=True)
 
 
 def unmask_attention_mask(mask, original_size):
--- finetune_compressv_vicuna7b_CLIP_mlp.sh
+++ finetune_compressv_vicuna7b_CLIP_mlp.sh
@@ -3,7 +3,7 @@
 export PJRT_DEVICE=TPU &&
 export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
-export CKPT_NAME="compressv_vicuna7b_CLIP_mlp_2scale_layer11_finetune_737k" &&
+export CKPT_NAME="compressv_vicuna7b_CLIP_mlp_2scale_layer11_finetune_2stagefix_737k" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -12,7 +12,7 @@ python cambrian/train/train_tpu.py \
     --version v1 \
     --data_path /mnt/disks/storage/data/finetune_data/jsons/737k.jsonl \
     --image_folder /mnt/disks/storage/data/finetune_data \
-    --pretrain_mm_mlp_adapter ./compressv_vicuna7b_CLIP_mlp_2scale_layer11_shareGPT4V_pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./compressv_vicuna7b_CLIP_mlp_2scale_layer11_shareGPT4V_pretrain_stage2fix/mm_projector.bin \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
     --max_num_image_crops 1 \
