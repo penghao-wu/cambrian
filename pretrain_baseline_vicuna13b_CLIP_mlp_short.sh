d633847cd6016f50990cb66b8828b23d565d8abc
penghaowu <wupenghaocraig@gmail.com>
2024-09-30 00:41:46 +0800

    debug

--- pretrain_baseline_vicuna13b_CLIP_mlp_short.sh
+++ pretrain_baseline_vicuna13b_CLIP_mlp_short.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="compressv_vicuna13b_CLIP_mlp_baseline_shareGPT4V_pretrain" &&
+export CKPT_NAME="compressv_vicuna13b_CLIP_mlp_baseline_shareGPT4V_resize_pretrain" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
