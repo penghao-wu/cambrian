d66103e8adcbf9e7efa3a46ae5dad00c418fe16b
penghaowu <wupenghaocraig@gmail.com>
2024-09-30 10:38:38 +0800

    debug

--- pretrain_compressv_vicuna7b_CLIP_mlp_short.sh
+++ pretrain_compressv_vicuna7b_CLIP_mlp_short.sh
@@ -37,7 +37,7 @@ python cambrian/train/train_tpu.py \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_aspect_ratio pad \
+    --image_aspect_ratio resize \
     --bf16 False \
     --output_dir $CKPT_DIR \
     --num_train_epochs 1 \
