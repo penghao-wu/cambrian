da4ff1576164472ba712f376cee57818b6d90176
penghaowu <wupenghaocraig@gmail.com>
2024-09-30 21:25:48 +0800

    debug

--- pretrain_compressv_vicuna7b_CLIP_mlp_short.sh
+++ pretrain_compressv_vicuna7b_CLIP_mlp_short.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="compressv_vicuna7b_CLIP_mlp_2scale_layer11_shareGPT4V_pad_pretrain_lr5e5" &&
+export CKPT_NAME="compressv_vicuna7b_CLIP_mlp_2scale_layer11_shareGPT4V_resize_pretrain_lr5e5" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -37,7 +37,7 @@ python cambrian/train/train_tpu.py \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_aspect_ratio pad \
+    --image_aspect_ratio resize \
     --bf16 False \
     --output_dir $CKPT_DIR \
     --num_train_epochs 1 \
