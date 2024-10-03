3248c87d957a1a0c8e700ffa24371bc76106d275
penghaowu <wupenghaocraig@gmail.com>
2024-10-04 00:26:49 +0800

    debug

--- finetune_compressv_sepkv_qwen05b_CLIP_mlp.sh
+++ finetune_compressv_sepkv_qwen05b_CLIP_mlp.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="compressv_qwen05b_CLIP_mlp_sepkv224_layer12_noinit_pad_finetune_738k_TPU" &&
+export CKPT_NAME="compressv_qwen05b_CLIP_mlp_sepkv224_layer14_noinit_pad_finetune_738k_TPU" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -21,7 +21,7 @@ python cambrian/train/train_tpu.py \
     --per_crop_token_len 576 \
     --compress_reduce_factor 4 \
     --compress_v True \
-    --compress_v_start_layer 12 \
+    --compress_v_start_layer 14 \
     --num_query_group 1 \
     --query_num_list '[576]' \
     --connector_depth 3 \
