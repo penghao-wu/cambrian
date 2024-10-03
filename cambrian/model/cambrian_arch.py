951b11ac717fec79de71e67f1f8c78a24c266c28
penghaowu <wupenghaocraig@gmail.com>
2024-10-03 20:08:56 +0800

    debug

--- cambrian/model/cambrian_arch.py
+++ cambrian/model/cambrian_arch.py
@@ -24,7 +24,7 @@ from ezcolorlog import root_logger as logger
 from .multimodal_encoder.builder import build_vision_tower_aux_list
 from .multimodal_projector.builder import build_vision_projector
 # from .vision_sampler import VisionTokenSampler, VisionMLP, VisionSA
-from .vision_mlp import VisionMLP, svd_initi
+from .vision_mlp import VisionMLP, svd_init
 
 from cambrian.constants import IGNORE_INDEX, IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 
