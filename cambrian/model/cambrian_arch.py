a4fa1ad8a2f2708e735898b1f68773dd15abfae2
penghaowu <wupenghaocraig@gmail.com>
2024-10-04 18:56:35 +0800

    debug

--- cambrian/model/cambrian_arch.py
+++ cambrian/model/cambrian_arch.py
@@ -228,10 +228,10 @@ class CambrianMetaModel:
 					self.vision_sampler_layers.load_state_dict(get_w(mm_projector_weights, 'vision_sampler_layers'),strict=True)
 				self.vision_query.data = mm_projector_weights['model.vision_query']
 			self.image_newline.data = mm_projector_weights['model.image_newline']
-			# if compress_v:
-			# 	for layer_idx in range(compress_v_start_layer, self.config.num_hidden_layers):
-			# 		incompatible_keys = self.layers[layer_idx].vision_mlp_layers.load_state_dict(get_w(mm_projector_weights, 'layers.{}.vision_mlp_layers'.format(layer_idx)),strict=True)
-			# 		print(f"Loaded vision mlp weights from {pretrain_mm_mlp_adapter}. Incompatible keys: {incompatible_keys}")
+			if compress_v:
+				for layer_idx in range(compress_v_start_layer, self.config.num_hidden_layers):
+					incompatible_keys = self.layers[layer_idx].vision_mlp_layers.load_state_dict(get_w(mm_projector_weights, 'layers.{}.vision_mlp_layers'.format(layer_idx)),strict=True)
+					print(f"Loaded vision mlp weights from {pretrain_mm_mlp_adapter}. Incompatible keys: {incompatible_keys}")
 
 				# incompatible_keys = self.vision_mlp_layers.load_state_dict(get_w(mm_projector_weights, "vision_mlp_layers"), strict=False)
 				# print(f"Loaded vision mlp weights from {pretrain_mm_mlp_adapter}. Incompatible keys: {incompatible_keys}")
