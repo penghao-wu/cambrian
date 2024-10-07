dde6a975407b32002adfa2dd0cd4c2f517847a3f
penghaowu <wupenghaocraig@gmail.com>
2024-10-07 19:47:31 +0800

    debug

--- cambrian/model/vision_mlp.py
+++ cambrian/model/vision_mlp.py
@@ -250,7 +250,7 @@ class VisionMLP_sa(nn.Module):
 		image_compress = image_compress.repeat_interleave(compress_reduce_factor, 1).repeat_interleave(compress_reduce_factor, 2).view(bs, self.num_heads, num_image_crops*side_len_full*side_len_full, -1)
 
 		image_full = image_full.view(bs*self.num_heads*num_image_crops, side_len_full, side_len_full, -1)
-		image_full_pool = self.pool(image_full.permute(0, 3, 1, 2)).permute(0, 2, 3, 1).view(bs, self.num_heads, num_image_crops*side_len_full*side_len_full, -1)
+		image_full_pool = self.pool(image_full.permute(0, 3, 1, 2)).permute(0, 2, 3, 1).repeat_interleave(compress_reduce_factor, 1).repeat_interleave(compress_reduce_factor, 2).view(bs, self.num_heads, num_image_crops*side_len_full*side_len_full, -1)
 		image_full = image_full.view(bs, self.num_heads, num_image_crops*side_len_full*side_len_full, -1)
 
 		gate_weight = self.gate(image_full)
