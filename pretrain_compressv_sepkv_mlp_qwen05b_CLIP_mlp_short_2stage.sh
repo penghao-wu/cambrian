fbb594914e007c03cca1b9036a8560fa2000fec1
penghaowu <wupenghaocraig@gmail.com>
2024-10-08 21:30:21 +0800

    debug

--- pretrain_compressv_sepkv_mlp_qwen05b_CLIP_mlp_short_2stage.sh
+++ pretrain_compressv_sepkv_mlp_qwen05b_CLIP_mlp_short_2stage.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="compressv_qwen05b_CLIP_mlp_2scaleffngate448_sepkv_ffn_layer12_shareGPT4V_square_pretrain_stage2joint" &&
+export CKPT_NAME="compressv_qwen05b_CLIP_mlp_sepsaffn_oproj448_2scalesagate_layer12_shareGPT4V_square_pretrain_stage2joint_TPU" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
