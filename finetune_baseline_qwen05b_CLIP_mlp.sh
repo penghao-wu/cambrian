bc997ca44b7867250b1bf62b5bde56f8ed5b7d9a
penghaowu <wupenghaocraig@gmail.com>
2024-10-09 14:03:14 +0800

    debug

--- finetune_baseline_qwen05b_CLIP_mlp.sh
+++ finetune_baseline_qwen05b_CLIP_mlp.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="compressv_qwen05b_CLIP_mlp_baseline_pad_masknoinit_layer14_finetune_738k_TPU" &&
+export CKPT_NAME="compressv_qwen05b_CLIP_mlp_baseline_resize_finetune_738k_TPU" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -33,7 +33,7 @@ python cambrian/train/train_tpu.py \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_aspect_ratio pad \
+    --image_aspect_ratio resize \
     --group_by_modality_length True \
     --bf16 False \
     --output_dir $CKPT_DIR \
