0ec9c5f1163fdc2254cee12b26b3a041f75f7dc0
penghaowu <wupenghaocraig@gmail.com>
2024-10-10 00:00:03 +0800

    debug

--- pretrain_cambrian_qwen05b_CLIP_mlp_compressv_short.sh
+++ pretrain_cambrian_qwen05b_CLIP_mlp_compressv_short.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="cambrian_qwen05b_CLIP_mlp_baseline_unpad_pretrain_old_TPU" &&
+export CKPT_NAME="cambrian_qwen05b_CLIP_mlp_2scale_dim448_layer12_shareGPT4V_pretrain_unpad_old_TPU" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -17,7 +17,7 @@ python cambrian/train/train_tpu.py \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
     --compress_v_start_layer 12 \
-    --compress_v False \
+    --compress_v True \
     --image_token_len 576 \
     --image_token_len_concise 36 \
     --num_query_group 1 \
