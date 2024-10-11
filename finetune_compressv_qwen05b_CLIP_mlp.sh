b5cb7ea9576b1b20070a6c3d087610333f2b66ce
penghaowu <wupenghaocraig@gmail.com>
2024-10-11 12:56:02 +0800

    debug

--- finetune_compressv_qwen05b_CLIP_mlp.sh
+++ finetune_compressv_qwen05b_CLIP_mlp.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="compressv_qwen05b_CLIP_mlp_2scaleold_layer12_stage2pt_resize_finetune_738k_TPU" &&
+export CKPT_NAME="compressv_qwen05b_CLIP_mlp_2scaleold_layer12_stage2pt_pad_finetune_738k_TPU" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -14,7 +14,7 @@ python cambrian/train/train_tpu.py \
     --version qwen_1_5 \
     --data_path ./llava_next_raw_format_processed.jsonl\
     --image_folder ./llava_next \
-    --pretrain_mm_mlp_adapter ./compressv_qwen05b_CLIP_mlp_2scaleold_layer12_shareGPT4V_resize_pretrain_stage2joint_TPU/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./compressv_qwen05b_CLIP_mlp_2scaleold_layer12_shareGPT4V_square_pretrain_stage2joint_TPU/mm_projector.bin \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
     --max_num_image_crops 1 \
@@ -36,7 +36,7 @@ python cambrian/train/train_tpu.py \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_aspect_ratio resize \
+    --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 False \
     --output_dir $CKPT_DIR \
--- pretrain_compressv_qwen05b_CLIP_mlp_short_2stage.sh
+++ pretrain_compressv_qwen05b_CLIP_mlp_short_2stage.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="compressv_qwen05b_CLIP_mlp_2scaleold_layer12_shareGPT4V_resize_pretrain_stage2joint_TPU" &&
+export CKPT_NAME="compressv_qwen05b_CLIP_mlp_2scaleold_layer12_shareGPT4V_square_pretrain_stage2joint_TPU" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -16,7 +16,7 @@ python cambrian/train/train_tpu.py \
     --image_folder /mnt/disks/storage/data/finetune_data \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
-    --pretrain_mm_mlp_adapter ./compressv_qwen05b_CLIP_mlp_baseline_shareGPT4V_resize_pretrain_TPU/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./compressv_qwen05b_CLIP_mlp_baseline_shareGPT4V_square_pretrain_TPU/mm_projector.bin \
     --max_num_image_crops 1 \
     --per_crop_token_len 576 \
     --compress_reduce_factor 4 \
