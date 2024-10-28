bfaabbb56ffac82f09e2496daf83bfecd259e3e0
penghaowu <wupenghaocraig@gmail.com>
2024-10-28 18:42:35 +0800

    debug

--- finetune_baseline_vicuna7b_CLIP_mlp.sh
+++ finetune_baseline_vicuna7b_CLIP_mlp.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="compressv_vicuna7b_CLIP_mlp_baseline_masknoinit_layer11_finetune_pad_738k" &&
+export CKPT_NAME="compressv_vicuna7b_CLIP_mlp_baseline_padPT_finetune_pad_738k" &&
 
 # export TPU_PROCESS_BOUNDS=1,1,1 &&
 # export TPU_VISIBLE_CHIPS=0 &&
@@ -17,7 +17,7 @@ python cambrian/train/train_tpu.py \
     --version v1 \
     --data_path ./llava_next_raw_format_processed.jsonl\
     --image_folder ./llava_next \
-    --pretrain_mm_mlp_adapter ./compressv_vicuna7b_CLIP_mlp_baseline_shareGPT4V_square_pretrain_TPU/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./compressv_vicuna7b_CLIP_mlp_baseline_shareGPT4V_pad_pretrain_TPU/mm_projector.bin \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
     --max_num_image_crops 1 \
