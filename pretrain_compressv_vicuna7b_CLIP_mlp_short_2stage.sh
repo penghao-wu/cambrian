35eed7e46289d4dc9b86f5d0fb6e26b3c3f7594f
penghaowu <wupenghaocraig@gmail.com>
2024-11-01 00:12:19 +0800

    debug

--- pretrain_compressv_vicuna7b_CLIP_mlp_short_2stage.sh
+++ pretrain_compressv_vicuna7b_CLIP_mlp_short_2stage.sh
@@ -5,7 +5,7 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export WANDB_API_KEY="618eb3b78242f01000855a123d29e2ac98a60f30" &&
 export WANDB_PROJECT="compressv" &&
-export CKPT_NAME="compressv_vicuna7b_CLIP_mlp_sepsa_prevalue_2scalecat_2convadd_oproj1024_layer0_shareGPT4V_square_pretrain_stage2joint_TPU" &&
+export CKPT_NAME="compressv_vicuna7b_CLIP_mlp_2scaleold_layer11_sepnorm_shareGPT4V_pad_pretrain_lr1e3_stage2joint_TPU" &&
 
 export CKPT_DIR="gs://cambrian-archive/checkpoints/$CKPT_NAME" &&
 
@@ -16,13 +16,13 @@ python cambrian/train/train_tpu.py \
     --image_folder /mnt/disks/storage/data/finetune_data \
     --vision_tower_aux_list '["openai/clip-vit-large-patch14-336"]' \
     --vision_tower_aux_token_len_list '[576]' \
-    --pretrain_mm_mlp_adapter ./compressv_vicuna7b_CLIP_mlp_baseline_shareGPT4V_square_pretrain_TPU/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./compressv_vicuna7b_CLIP_mlp_baseline_shareGPT4V_pad_pretrain_TPU/mm_projector.bin \
     --max_num_image_crops 1 \
     --per_crop_token_len 576 \
     --compress_reduce_factor 4 \
     --compress_v True \
-    --compress_v_start_layer 0 \
-    --mm_vision_mlp_lr 1e-4 \
+    --compress_v_start_layer 11 \
+    --mm_vision_mlp_lr 1e-3 \
     --num_query_group 1 \
     --query_num_list '[576]' \
     --connector_depth 3 \
@@ -33,12 +33,12 @@ python cambrian/train/train_tpu.py \
     --start_of_vision_sampler_layers 0 \
     --stride_of_vision_sampler_layers 3 \
     --mm_projector_type mlp2x_gelu \
-    --mm_vision_sampler_lr 1e-4 \
+    --mm_vision_sampler_lr 1e-3 \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_aspect_ratio resize \
+    --image_aspect_ratio pad \
     --bf16 False \
     --output_dir $CKPT_DIR \
     --num_train_epochs 1 \
@@ -49,7 +49,7 @@ python cambrian/train/train_tpu.py \
     --save_strategy "steps" \
     --save_steps 100000 \
     --save_total_limit 1 \
-    --learning_rate 1e-4 \
+    --learning_rate 1e-3 \
     --weight_decay 0. \
     --warmup_ratio 0.06 \
     --lr_scheduler_type "cosine" \
